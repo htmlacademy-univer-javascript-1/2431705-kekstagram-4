08399494c9e5b0b27b1375c6b8d370c193d3bfff
PavelUd <paveludin4@gmail.com>
2023-09-28 23:44:44 +0500

    micro fix

--- js/functions.js
+++ js/functions.js
@@ -1,15 +1,20 @@
 const checkLengthString =(str, length) =>
   str.length >= Number(length);
+
 const isPalindrome = (str) =>
 {
-  str  = String(str).replaceAll(' ', '').toLowerCase();
+  str = String(str).replaceAll(' ', '').toLowerCase();
   const strReverse = str.split('').reverse().join('');
   return str === strReverse ;
 };
 
-const getNumberString = (str) =>
+const getNumberFromString = (str) =>
 {
   str = str.toString();
   const array =  Array.from(str.split('')).filter((char) => !isNaN(parseInt(char, 10)));
   return array.length > 0 ? +(array.join('')) : NaN;
 };
+
+checkLengthString('aboba', 8);
+getNumberFromString('aboba228');
+isPalindrome('aboba');
