aa723dd7dd176aa085fecd97d48bf8951c8298b2
PavelUd <paveludin4@gmail.com>
2023-10-02 20:03:29 +0500

    fix

--- js/functions.js
+++ js/functions.js
@@ -1,15 +1,13 @@
-const checkLengthString =(str, length) =>
+const checkLengthString = (str, length) =>
   str.length >= Number(length);
 
-const isPalindrome = (str) =>
-{
+const isPalindrome = (str) => {
   str = String(str).replaceAll(' ', '').toLowerCase();
   const strReverse = str.split('').reverse().join('');
   return str === strReverse ;
 };
 
-const getNumberFromString = (str) =>
-{
+const getNumberFromString = (str) => {
   str = str.toString();
   const array =  Array.from(str.split('')).filter((char) => !isNaN(parseInt(char, 10)));
   return array.length > 0 ? +(array.join('')) : NaN;
