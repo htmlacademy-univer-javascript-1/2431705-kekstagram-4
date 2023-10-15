ba9c0f66ab1a6bb92e2b66b7b5dbc979878ebc22
PavelUd <paveludin4@gmail.com>
2023-10-15 23:08:05 +0500

    второе задание

--- js/functions.js
+++ js/functions.js
@@ -16,3 +16,20 @@ const getNumberFromString = (str) => {
 checkLengthString('aboba', 8);
 getNumberFromString('aboba228');
 isPalindrome('aboba');
+
+const getMinutesFromTime = (time) => {
+  const [hours, minutes] = time.split(':').map(Number);
+  return hours * 60 + minutes;
+};
+
+const isMeetingDuringWorkingHours = (start, finish, startMeeting, meetingTime) =>{
+  const [startInMinutes, startMeetingInMinutes, finishInMinutes] = [start, startMeeting, finish].map(getMinutesFromTime);
+
+  return (startInMinutes <= startMeetingInMinutes) && (startMeetingInMinutes + meetingTime <= finishInMinutes);
+
+};
+console.log(isMeetingDuringWorkingHours('08:00', '17:30', '14:00', 90),
+  isMeetingDuringWorkingHours('8:0', '10:0', '8:0', 120),
+  isMeetingDuringWorkingHours('08:00', '14:30', '14:00', 90),
+  isMeetingDuringWorkingHours('14:00', '17:30', '08:0', 90),
+  isMeetingDuringWorkingHours('8:00', '17:30', '08:00', 900));
