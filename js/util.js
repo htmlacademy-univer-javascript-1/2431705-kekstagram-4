91447718427f6375c5e1893ccfbad2a219e15ef9
PavelUd <paveludin4@gmail.com>
2023-10-15 22:52:13 +0500

    не сохранился

--- js/util.js
+++ js/util.js
@@ -1,3 +1,3 @@
 const getRandomNumberFromInterval = (start, end)=>
   Math.ceil(Math.random() * (end - start + 1)) + (start - 1);
-  export {getRandomNumberFromInterval}
+export {getRandomNumberFromInterval};
