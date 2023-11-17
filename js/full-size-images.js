8ce17eb76f6979506ff7ce3d9e09abad2689ddf0
PavelUd <paveludin4@gmail.com>
2023-11-18 03:22:16 +0500

    опа новое обновление

--- js/full-size-images.js
+++ js/full-size-images.js
@@ -1,8 +1,13 @@
+const LIMIT = 5;
 const bigPicture = document.querySelector('.big-picture');
 const commentsCount = bigPicture.querySelector('.comments-count');
 const likesCount = bigPicture.querySelector('.likes-count');
 const bigImage = bigPicture.querySelector('.big-picture__img img');
 const description = bigPicture.querySelector('.social__caption');
+const uploadMoreButton = bigPicture.querySelector('.comments-loader');
+const nowComments = bigPicture.querySelector('.openedComments-count');
+const commentsBlock = bigPicture.querySelector('.social__comments');
+
 
 const createComment = (avatar, name, text) => `<li class="social__comment">
     <img
@@ -14,10 +19,31 @@ const createComment = (avatar, name, text) => `<li class="social__comment">
   </li>`;
 
 
+const uploadComments = (limit, comments, end) => {
+  const start = end - limit;
+  if(end >= comments.length){
+    end = comments.length;
+    uploadMoreButton.hidden = true;
+  }
+
+  nowComments.textContent = end === comments.length - 1 ? end + 1 : end;
+  commentsBlock.insertAdjacentHTML(
+    'afterbegin',
+    comments.slice(start, end).map((comment) => createComment(comment.avatar, comment.name, comment.message)).join('')
+  );
+};
+
+
 const createCommentsBlock = (comments) =>{
-  const commentsBlock = bigPicture.querySelector('.social__comments');
   commentsBlock.innerHTML = '';
-  commentsBlock.insertAdjacentHTML('afterbegin', comments.map((comment) => createComment(comment.avatar, comment.name, comment.message)).join(''));           // Array(comments).map
+  let end = LIMIT;
+
+  uploadComments(LIMIT, comments, end);
+
+  uploadMoreButton.onclick = () =>{
+    end += LIMIT;
+    uploadComments(LIMIT, comments, end);
+  };
 };
 
 
@@ -26,22 +52,23 @@ const constructBigPicture = (picture, photos) =>{
   const comments = picture.querySelector('.picture__comments');
   const image = picture.querySelector('.picture__img');
   const photoInfo = photos.find((photo) => `/${photo.url}` === new URL(image.src).pathname);
+
   description.textContent = photoInfo.description;
   likesCount.textContent = likes.textContent;
   bigImage.src = image.src;
   commentsCount.textContent = comments.textContent;
+
   createCommentsBlock(photoInfo.comments, bigPicture);
 };
 
-
 export const showBigPicture = (photos) => {
+
   const pictures = document.querySelectorAll('.picture');
   for (const picture of pictures){
+
     picture.onclick = () =>{
-      constructBigPicture(picture, photos);
       bigPicture.classList.remove('hidden');
-      bigPicture.querySelector('.social__comment-count').hidden = true;
-      bigPicture.querySelector('.comments-loader').hidden = true;
+      constructBigPicture(picture, photos);
       document.body.classList.add('modal-open');
     };
   }
